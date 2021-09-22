9bfcdb06346188d583c5e6893f969759b77be19b
Reynaldo Cauche <reynaldo1994@hotmail.com>
2021-09-22 15:32:36 -0500

    feat: Creating the input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import './style.scss'
+
+export default function Input ({
+  type = 'text',
+  initialValue = '',
+  placeholder = '',
+  style = {},
+  min = 0,
+  max = 99999,
+  onChange = () => { }
+}) {
+
+  const [value, setValue] = useState(initialValue)
+
+  const onKeyDown = (event) => {
+    if (type === 'number') {
+      if (event.keyCode === 69 || event.keyCode === 189) event.preventDefault()
+    }
+  }
+
+  const onChangeHandler = (event) => {
+    const val = event?.target?.value ?? ""
+    setValue(val)
+    onChange(val)
+  }
+
+  return (
+    <div>
+      <input
+        className='Input'
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChangeHandler}
+        onKeyDown={onKeyDown}
+        style={style}
+        min={min}
+        max={max}
+      />
+    </div>
+  )
+}
