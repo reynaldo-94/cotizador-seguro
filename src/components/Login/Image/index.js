29458b663a7043713fabd08925576684176ae64b
Reynaldo Cauche <reynaldo1994@hotmail.com>
2021-09-30 14:22:55 -0500

    refactor: Rename component classes

--- src/components/Login/Image/index.js
+++ src/components/Login/Image/index.js
@@ -2,27 +2,20 @@ import React from 'react'
 import Logo from '../../Header/Logo'
 import background from '../../../assets/login/background.png'
 import imagenLogin from '../../../assets/login/login-logo.png'
+import './style.scss'
 
 export default function Image() {
   return (
     <div>
-      <img src={background} />
-      <div style={{
-        position: 'absolute',
-        top: 30,
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 20,
-        paddingLeft: 100,
-        width: 320
-      }}>
+      <img src={background} alt='Background' />
+      <div className='blockImage'>
         <Logo />
-        <img src={imagenLogin} />
-        <div style={{ display: 'flex', flexDirection: 'column'}}>
-          <span style={{ color: '#494f66', fontSize: 12 }}>¡NUEVO!</span>
-          <div style={{ display: 'flex', flexDirection: 'column', gap: 18 }}>
-            <h1 style={{ margin: 0, fontSize: 36, lineHeight: '48px' }}>Seguro <span style={{ color: '#ef3340' }}>Vehicular <br />Tracking</span></h1>
-            <p style={{ margin: 0, color: '#676f8f', fontSize: 14 }}>Cuentamos donde le haras seguimiento a tu seguro</p>
+        <img src={imagenLogin} alt='Image Login' />
+        <div className='image'>
+          <span className='image__span'>¡NUEVO!</span>
+          <div className='image__block'>
+            <h1 className='image__title'>Seguro <span className='image__title--color'>Vehicular <br />Tracking</span></h1>
+            <p className='image__description'>Cuentamos donde le haras seguimiento a tu seguro</p>
           </div>
         </div>
       </div>
